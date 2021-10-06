e8f94416306c80802bf8a9423c208b479e276a7f
OberdanSoldi <oberdan_soldi@outlook.com.br>
2021-10-06 19:41:30 -0300

    nono commit

--- exercicios-js/EstruturasDeControle/1-Estrutura-IF/1-Estrutura-IF.js
+++ exercicios-js/EstruturasDeControle/1-Estrutura-IF/1-Estrutura-IF.js
@@ -1,20 +1,34 @@
-function boaNoticia(nota) {
-    if(nota > 7) {
-        console.log(`Aprovado com ${nota}`);
+// // Primeira parte
+
+// function boaNoticia(nota) {
+//     if(nota > 7) {
+//         console.log(`Aprovado com ${nota}`);
+//     };
+// };
+
+// boaNoticia(8.1);
+// boaNoticia(6.1);
+
+// function verdade(valor) {
+//     if(valor) {
+//         console.log(`sim ${valor} é verdadeiro`);
+//     };
+// };
+
+// verdade();
+// verdade(null);
+// verdade(NaN);
+// verdade('')
+// verdade(-1);
+
+// Segunda parte
+
+function teste1(num) {
+    if(num > 7) {
+        console.log(num);
+        console.log('Final')
     };
 };
 
-boaNoticia(8.1);
-boaNoticia(6.1);
-
-function verdade(valor) {
-    if(valor) {
-        console.log(`sim ${valor} é verdadeiro`);
-    };
-};
-
-verdade();
-verdade(null);
-verdade(NaN);
-verdade('')
-verdade(-1);
+console.log(teste1(6));
+console.log(teste1(8));
---
+++ exercicios-js/EstruturasDeControle/2-Estrutura-IF-ELSE/2-Estrutura-IF-ELSE.js
@@ -0,0 +1,9 @@
+const Resultado = function(nota) {
+    if(nota > 7) {
+        console.log('Aprovado');
+    }else{
+        console.log('Reprovado');
+    };
+};
+
+Resultado('teste');
